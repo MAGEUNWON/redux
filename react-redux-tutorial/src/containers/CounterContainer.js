6722f4a1a8e7c5990bbab16a2afd20c9c01bf174
mageunwon <geunwon1947@gmail.com>
2023-04-19 22:59:48 +0900

    리덕스 적용 완료

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,3 +1,5 @@
+// import { bindActionCreators } from 'redux';
+
 import {connect} from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
@@ -8,22 +10,32 @@ const CounterContainer = ({number, increase, decrease}) => {
   );
 };
 
-const mapStateToProps = state => ({
-  number: state.counter.number,
-});
-const mapDispatchToProps = dispatch => ({
-  //임시 함수
-  increase: () => {
-    // console.log('increase');
-    dispatch(increase());
-  },
-  decrease: () => {
-    // console.log('decrease');
-    dispatch(decrease());
-  },
-});
-
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
+  state => ({
+    number: state.counter.number,
+  }),
+  {
+    increase,
+    decrease,
+  },
 )(CounterContainer);
+
+// const mapStateToProps = state => ({
+//   number: state.counter.number,
+// });
+// const mapDispatchToProps = dispatch => ({
+//   //임시 함수
+//   increase: () => {
+//     // console.log('increase');
+//     dispatch(increase());
+//   },
+//   decrease: () => {
+//     // console.log('decrease');
+//     dispatch(decrease());
+//   },
+// });
+
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps,
+// )(CounterContainer);
